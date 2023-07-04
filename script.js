ba80291c8348562fbedb56d102f74b7e155321d7
Luisdoom <luisesquivel97@hotmail.com>
2023-07-03 19:39:13 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,11 @@
 
     if (window.matchMedia("(orientation:landscape)").matches) {
       $("#myVideo").html(
-        '<source src="https://eachevery.s3.amazonaws.com/markseliger/Bckg_16x9_Fnl.mp4" type="video/mp4"></source>'
+        '<source src="https://vod-progressive.akamaized.net/exp=1688448784~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F1044%2F19%2F480223896%2F2146728295.mp4~hmac=9a63cec40d8ffa159e4b670eb5b38779e2b426fa28fe0aacd7492c246591b618/vimeo-prod-skyfire-std-us/01/1044/19/480223896/2146728295.mp4" type="video/mp4"></source>'
       );
     } else {
       $("#myVideo").html(
-        '<source src="https://eachevery.s3.amazonaws.com/markseliger/Bckg_9x16_Fnl-_1.mp4" type="video/mp4"></source>'
+        '<source src="https://www.bigbuckbunny.org/"></source>'
       );
     }
 
