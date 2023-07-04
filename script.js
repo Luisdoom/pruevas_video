6b1924a88b714779a73b45b90614c34a7ae53526
Luisdoom <luisesquivel97@hotmail.com>
2023-07-03 19:20:23 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-
 // Get the video
 
-var video = document.getElementById("myVideo");
 
 
-
-
-
-function videosR(){
- if (window.matchMedia("(orientation:landscape)").matches) {
-                     
-   $("#myVideo").html('<source src="https://eachevery.s3.amazonaws.com/markseliger/Bckg_16x9_Fnl.mp4" type="video/mp4"></source>' );
-   
-                    } 
-                      
-                        else{
-                         $("#myVideo").html('<source src="https://eachevery.s3.amazonaws.com/markseliger/Bckg_9x16_Fnl-_1.mp4" type="video/mp4"></source>' );
+function videosR() {
+    if (window.matchMedia("(orientation:landscape)").matches) {
+      $("#myVideo").html(
+        '<source src="https://eachevery.s3.amazonaws.com/markseliger/Bckg_16x9_Fnl.mp4" type="video/mp4"></source>'
+      );
+      lock(orientation);
+    } else {
+      $("#myVideo").html(
+        '<source src="https://eachevery.s3.amazonaws.com/markseliger/Bckg_9x16_Fnl-_1.mp4" type="video/mp4"></source>'
+      );
+      lock(orientation);
+    }
+  }
   
-                        }}
-
+  
+  
+  videosR();
