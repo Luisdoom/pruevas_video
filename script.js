101d3fa9ea9d9d6ec27ffcf26261a0c576463118
Luisdoom <luisesquivel97@hotmail.com>
2023-07-03 19:23:06 -0600

    push push  push

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ function videosR() {
       $("#myVideo").html(
         '<source src="https://eachevery.s3.amazonaws.com/markseliger/Bckg_16x9_Fnl.mp4" type="video/mp4"></source>'
       );
-      lock(orientation);
+     
     } else {
       $("#myVideo").html(
         '<source src="https://eachevery.s3.amazonaws.com/markseliger/Bckg_9x16_Fnl-_1.mp4" type="video/mp4"></source>'
       );
-      lock(orientation);
+      
     }
   }
   
