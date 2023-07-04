4bb331fda1a14e49cdc35d9e7cf0b99f56c139a6
Luisdoom <luisesquivel97@hotmail.com>
2023-07-03 19:17:44 -0600

    mmmm

--- script.js
+++ script.js
@@ -3,12 +3,19 @@
 
 var video = document.getElementById("myVideo");
 
-if (window.innerWidth > window.innerHeight || window.innerWidth > 960) {
-    var video = document.getElementById("myVideo");
-  } else if(window.matchMedia("(orientation:landscape)").matches) {
-    var video = document.getElementById("myVideo2");
-}
-      else{
-        var video = document.getElementById("myVideo2");
-      }
+
+
+
+
+function videosR(){
+ if (window.matchMedia("(orientation:landscape)").matches) {
+                     
+   $("#myVideo").html('<source src="https://eachevery.s3.amazonaws.com/markseliger/Bckg_16x9_Fnl.mp4" type="video/mp4"></source>' );
+   
+                    } 
+                      
+                        else{
+                         $("#myVideo").html('<source src="https://eachevery.s3.amazonaws.com/markseliger/Bckg_9x16_Fnl-_1.mp4" type="video/mp4"></source>' );
   
+                        }}
+
