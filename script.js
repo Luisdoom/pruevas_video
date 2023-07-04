74f417b9e1aab3c214b5c43858bab23b02e15598
Luisdoom <luisesquivel97@hotmail.com>
2023-07-03 18:42:36 -0600

    mUCHOS CAMPUOS

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 
 // Get the video
+
 var video = document.getElementById("myVideo");
 
+if (window.innerWidth > window.innerHeight || window.innerWidth > 960) {
+    var video = document.getElementById("myVideo");
+  } else if(window.matchMedia("(orientation:landscape)").matches) {
+    var video = document.getElementById("myVideo2");
+}
+      else{
+        var video = document.getElementById("myVideo2");
+      }
+  
